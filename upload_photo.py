ce12ffe7a5f201696af3fc4202baf17785ffe9ef
Keeeril <keeeril@Keeeril-2.local>
2022-04-28 23:37:27 +0300

    delete token

--- upload_photo.py
+++ upload_photo.py
@@ -2,11 +2,13 @@ from random import randrange
 from city import get_city
 from photos_get import get_photo
 import requests
+with open('/Users/keeeril/Desktop/Netology/Token/Token_group.txt', 'rt', encoding='utf-8') as file:
+    token_group = file.readline()
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = 'c3565f4fff3ad1de652be3bd3651ad015965d8bdb1cc64aa46b3cdadea269c798c99d202f90732e29a999'
+token = token_group
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
